5d9a377bd27301dc96881ae174772a94c5c1cf17
Darius <dariusmmcclendon@gmail.com>
2022-04-07 14:40:08 -0400

    Added code for store displays

--- src/components/Stores.js
+++ src/components/Stores.js
@@ -8,8 +8,43 @@ export default function Stores(){
         <div>
             <h2 className="text-center">Choose your store in <span style={{color: "green"}}>Atlantic City</span></h2>
             <Container>
-                <Row>
+                <Row className="border-bottom pb-2 pt-2">
                     <Col>
+                        <Container className="border rounded m-1 p-2">
+                            <img src="/BJs-Logo.png" alt="logo" style={{width : "50px", height : "50px"}}/> BJ's Wholesale
+                        </Container>
+                       
+                    </Col>
+                    <Col>
+                        <Container className="border rounded m-1 p-2">
+                            <img src="/Walgreens-Logo.png" alt="logo" style={{width : "50px", height : "50px"}}/> Walgreen's
+                        </Container>
+                    </Col>
+                    <Col>
+                        <Container className="border rounded m-1 p-2">
+                            <img src="/Costco-Logo.png" alt="logo" style={{width : "50px", height : "50px"}}/> Costco
+                        </Container>
+
+                    </Col>
+                </Row>
+                
+                <Row className="border-bottom pb-2 pt-2">
+                    <Col>
+                        <Container className="border rounded m-1 p-2">
+                            <img src="/BJs-Logo.png" alt="logo" style={{width : "50px", height : "50px"}}/> BJ's Wholesale
+                        </Container>
+                       
+                    </Col>
+                    <Col>
+                        <Container className="border rounded m-1 p-2">
+                            <img src="/Walgreens-Logo.png" alt="logo" style={{width : "50px", height : "50px"}}/> Walgreen's
+                        </Container>
+                    </Col>
+                    <Col>
+                        <Container className="border rounded m-1 p-2">
+                            <img src="/Costco-Logo.png" alt="logo" style={{width : "50px", height : "50px"}}/> Costco
+                        </Container>
+
                     </Col>
                 </Row>
             </Container>
